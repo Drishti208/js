b07e6f770d514b3b3cbbce74b71bb929c06a8e09
Drishti208 <dgupta4_be25@thapar.edu>
2026-06-04 07:20:27 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js cide as new version 
+// alert(3+3) won't work as we are using node js and not browser
+
+// datatypes
+// number
+// bigint
+// string =""
+// boolean = true/false
+// null= standalone value
+// undefined
+// symbol = uniqueness
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);//object (imp interview ques)
---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+let accountId = 1234
+let accountName = "Ramesh"
+const accountpass = 2564
+
+console.table([accountId,accountName,accountpass]);
+/* prefer not to use var because of issue in block scope and functional scope*/
+
