7530df88de0be7fa6eb6feffcbe633551ac20c35
Drishti208 <dgupta4_be25@thapar.edu>
2026-06-04 16:03:40 +0000

    conversions

---
+++ 01_basics/conversions & operations.js
@@ -0,0 +1,24 @@
+let score=33;
+let valueInNumber = Number(score);
+console.log(typeof score);
+console.log(typeof valueInNumber );
+console.log(valueInNumber);
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1
+
+let isLoggedIn=1
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanLoggedIn);
+console.log(booleanLoggedIn );
+
+// 1=>true 0=>false
+// ""=>false
+// "drishti"=>true
+
+// similarly do number to string conversion 
+
+
+
+
