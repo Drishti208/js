314d5591f8b35e02bd5b2e8bde8b39337041a43a
Drishti208 <dgupta4_be25@thapar.edu>
2026-06-04 16:44:35 +0000

    operations

--- 01_basics/conversions & operations.js
+++ 01_basics/conversions & operations.js
@@ -17,7 +17,31 @@ console.log(booleanLoggedIn );
 // ""=>false
 // "drishti"=>true
 
-// similarly do number to string conversion 
+// similarly do number to string conversion
+
+// *********operators**********
+console.log("1"+2+3); output = 123 //it reads from left to right so it treats it as string as string comes first
+console.log("1"+"2"+3); output = 123
+console.log(1+2+"3"); output = 33
+console.log(1+2+3); output = 6
+
+console.log(+true); //the output will be 1  
+console.log(+""); //the output will be 0
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3" see first y will be assigned the value of x then x=x+1 operation will take place ie first assign then add this is prefix
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"see first x=x+1 operation will take place then that value will be given to y ie first add then assign this is postfix
+
+
+
 
 
 
