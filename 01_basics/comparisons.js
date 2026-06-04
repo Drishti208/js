04c953cf25c4ae579c5911e59bae6f3a0de07f35
Drishti208 <dgupta4_be25@thapar.edu>
2026-06-04 17:24:57 +0000

    comparison

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,25 @@
+//if we compare null and 0 then comparison converts null into 0
+console.log(null>0); //output = false
+console.log(null>=0); //output = true
+console.log(null==0); //output = false
+// in case of undefined three of the cases will give output as false
+
+//summary: avoid comparing two diff data types. === compares data types as well
+
+//********SUMMARY OF DATATYPES*******//
+
+//JavaScript is a dynamically typed language. This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it. You can assign different types of values to a variable during its lifetime.
+//Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
